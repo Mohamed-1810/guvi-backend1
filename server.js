80a7d8783ebef5a758af555bb65ee7edd956dfa0
Mohamed-1810 <mohdtharik.cs@gmail.com>
2023-06-04 13:23:29 +0530

    finalfour

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://guvi-task3.netlify.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', 'true'); // Include this line
+  
     next();
   });
 const db = mysql.createConnection({
