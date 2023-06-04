91e5060d851d95626ca90310a827a4adef0ac7c5
Mohamed-1810 <mohdtharik.cs@gmail.com>
2023-06-04 13:18:17 +0530

    finalthree

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-    {
-        // origin: ["http://localhost:3000"],
-        origin: ["https://guvi-task3.netlify.app/"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+// app.use(cors(
+//     {
+//         // origin: ["http://localhost:3000"],
+//         origin: ["https://guvi-task3.netlify.app/"],
+//         methods: ["POST", "GET"],
+//         credentials: true
+//     }
+// )); 
 
 // app.use(cors());
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://guvi-task3.netlify.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
